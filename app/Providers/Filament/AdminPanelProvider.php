50d363fbe07ba921f8cd8bdb67a135edfb97d425
mailinh2101 <2007210936@hufi.edu.vn>
2026-06-12 16:53:36 +0700

    feat: enable default profile page in Filament for password changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->brandName('GREECO Admin')
             ->font('Outfit')
             ->sidebarCollapsibleOnDesktop()
